ca32afee026dc7adb05e5905c04371778ed976b1
Sophia Miao <sophia@miao.fm>
2026-04-23 09:48:57 -0400

    still need to fix app

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Set "@" to refer to the root folder
+      '@views': path.resolve(__dirname, 'src/views'),
+    },
+  },
+});
