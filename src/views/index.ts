5893a96344ab623d321860837818010fbd3c6254
Sophia Miao <sophia@miao.fm>
2026-04-30 09:30:51 -0400

    change folder name

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,17 +1,17 @@
-export * from '@/views/movies/CreditsView';
-export * from '@/views/movies/EpisodeView';
+export * from '@/views/media/CreditsView';
+export * from '@/views/media/EpisodeView';
 export * from '@/views/site/ErrorView';
-export * from '@/views/movies/GenreView';
+export * from '@/views/media/GenreView';
 export * from '@/views/site/HomeView';
-export * from '@/views/movies/ImagesView';
-export * from '@/views/movies/MoviesView';  
-export * from '@/views/movies/PersonView';
-export * from '@/views/movies/ReviewsView';
-export * from '@/views/movies/SearchView';
-export * from '@/views/movies/SeasonsView';
-export * from '@/views/movies/TelevisionView';
-export * from '@/views/movies/TrailersView';
-export * from '@/views/movies/TrendingView';
+export * from '@/views/media/ImagesView';
+export * from '@/views/media/MoviesView';  
+export * from '@/views/media/PersonView';
+export * from '@/views/media/ReviewsView';
+export * from '@/views/media/SearchView';
+export * from '@/views/media/SeasonsView';
+export * from '@/views/media/TelevisionView';
+export * from '@/views/media/TrailersView';
+export * from '@/views/media/TrendingView';
 
 
 
--- src/views/media/SearchView.tsx
+++ src/views/media/SearchView.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent } from 'react';
+import { type ChangeEvent } from 'react';
 
 type SearchBarProps = {
   value: string;
